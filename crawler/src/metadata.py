811cc510a824f86f1128c4986ec2f708a07eb79c
PeterHsiao <u09800198@gmail.com>
2016-11-17 22:44:58 +0800

    修正目錄結構V2

--- crawler/src/metadata.py
+++ crawler/src/metadata.py
@@ -3,13 +3,15 @@ import json
 import requests
 import logging
 import os
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 LOGGING_FILE = 'ipgod.log'
 logging.basicConfig(#filename=LOGGING_FILE,
                     level=logging.INFO,
                     format='%(asctime)s [%(levelname)s] %(filename)s_%(lineno)d  : %(message)s')
 logger = logging.getLogger('root')
-
 
 class Metadata(object):
     
@@ -25,45 +27,46 @@ class Metadata(object):
         if x.get("success") == False:
             return []
         
-        Metadata.getLogFile(x)
+        Metadata.getLogFile(x, dataid)
+        # print(dataid)
         
         result = []
         
         for element in x["result"]["distribution"]:
-            obj = Metadata(element, x["result"]["organization"], x["result"]["title"])
+            obj = Metadata(element)
             result.append(obj)
         
         #result.append(x["result"]["organization"])
+        
         return result
     
     
-    def __init__(self, json, organization, title):
+    def __init__(self, x):
         
-        self.title = title.replace("/","").rstrip()
-        self.organization = organization
-        self.resourceID = json.get('resourceID')
+        self.resourceID = x.get('resourceID')
         # eg. resourceID = A59000000N-000229-001
         #     datasetID = A59000000N-000229
         self.datasetID = self.resourceID[:-4]
-        self.resourceDescription = json.get('resourceDescription')
-        self.format = json.get('format',"NA")
+        self.resourceDescription = x.get('resourceDescription')
+        self.format = x.get('format',"NA")
         if self.format == "NA":
             logger.warn("No format")
-        self.resourceModified = json.get('resourceModified')
+        self.resourceModified = x.get('resourceModified')
     
         # Some data has no downloadURL field
-        if 'downloadURL' in json:
-            self.downloadURL = json['downloadURL']
-            
-        self.metadataSourceOfData = json['metadataSourceOfData']
-        self.characterSetCode = json['characterSetCode']
+        if 'downloadURL' in x:
+            self.downloadURL = x['downloadURL']
+        if 'accessURL' in x:
+            self.accessURL = x['accessURL']
+        
+        self.metadataSourceOfData = x['metadataSourceOfData']
+        self.characterSetCode = x['characterSetCode']
         
         
     def getDataID(self):
         return self.dataid
     
-    def download(self, URLType):
-        # TODO: implement resource download logic
+    def download(self):
         """
         Download data via self.downloadURL field
         return True if download complete, False if download fail
@@ -71,25 +74,31 @@ class Metadata(object):
         if URLType == 0, URL is downloadURL. else if URLType is 1, URL is accessURL
         """
         if not hasattr(self, 'downloadURL') and hasattr(self, 'accessURL'):
-            logger.warn(self.resourceID + " NO download URL")
+            logger.warn(self.resourceID + " NO download URL and NO access URL")
             return False
         else:
-            if URLType == 0:
+            if hasattr(self, 'downloadURL'):
                 URL = self.downloadURL
-            elif URLType == 1:
+            elif hasattr(self, 'accessURL'):
                 URL = self.accessURL
-            logger.info("download from " + self.downloadURL)
+            logger.info("download from " + URL)
             
             name = self.resourceID.replace("/","")
-            dir_name = self.organization+"\\"+self.title
-            
-            abspath = os.path.abspath(dir_name)+"\\"
-            
-            
-            response = requests.get(URL,stream=True)
+            dir_name = self.datasetID+"/"
+                        
+            abspath = const.DOWNLOAD_PATH+"/"+dir_name
             
             
-            if "zip" in sURL.lower():
+            response = requests.get(URL,stream=True,verify=False)
+            # TODO: Save download status in postgreSQL DB
+            # postgreSQL schema
+            # dataset-id(resourceID), download-timestamp, download-status
+            
+            self.downloadStatusCode = response.status_code
+            if self.downloadStatusCode/100 >= 4:
+                return False
+            
+            if "zip" in URL.lower():
                 fileTypeFromURL = "zip"
             elif "csv" in URL.lower():
                 fileTypeFromURL = "csv"
@@ -129,15 +138,19 @@ class Metadata(object):
                 fileType = fileTypeFromContentType
             elif fileTypeFromFormat != "NA":
                 fileType = fileTypeFromFormat
-
-            file_name = abspath + name+"." + fileType
-            print(file_name)
+            
+            file_name = abspath + name +"." + fileType.strip(";\"")
+            
             with open(file_name, 'wb') as f:
                 for chunk in response.iter_content(chunk_size=1024): 
                     if chunk:
                         f.write(chunk)
+            logger.info("Download completed. File path: "+file_name)
             
             return True
+    
+    def getDownloadStatusCode(self):
+        return self.downloadStatusCode
     
     def getOrganization(self):
         return self.organization
@@ -166,46 +179,34 @@ class Metadata(object):
     def getCharacterSetCode(self):
         return self.characterSetCode
     
-    def getLogFile(x):
-        '''
-        This function will create a directory and a log file which under former directory.
-        A directory file will be named by the owner name while is the key "organization"'s value in json
-        A log file will be named by file title in json
-        '''
+    def getLogFile(x, dataid):
+        #DONE: Just save the JSON object
+        # Just save the JSON object as a json file which is entitled by datasetID
+        # eg. A59000000N-000229.json
+      
         # get log file name
-        name = x["result"].get("title","NOtitle")
+        datasetID = dataid
+        
+        name = datasetID
         name = name.replace("/","").rstrip()
         
         # get directory name
-        dir_name = x["result"]["organization"].rstrip()
+        dir_name = datasetID.rstrip()
         
+        path = const.DOWNLOAD_PATH + "/"
+        dir_name = path + dir_name
+        logger.info("Create a directory. Directory path: "+dir_name)
         # create a organization directory in current path
         if not os.path.exists(dir_name):
             os.makedirs(dir_name)
         
-        # create a file title directory under organization directory 
-        if not os.path.exists(dir_name+"\\"+name):
-            os.makedirs(dir_name+"\\"+name)
-        
         # abspath will be the log file's path
-        abspath = os.path.abspath(dir_name)+"\\"+name+"\\"
+        # abspath = os.path.abspath(dir_name)+"\\"+name+"\\"
         
         # get log file absolute path
-        file_name = abspath + name + ".txt"
+        file_name = dir_name + "/" + name + ".json"
         
         # open a file IO stream and write result
         f = open(file_name, "w", encoding = "UTF-8")
-        
-        for k, v in x["result"].items():
-            # print(k,type(v))
-            if type(v) is list:
-                f.write("%-25s: \n" %(k))
-                for j in v:
-                    if type(j) is dict:
-                        for distribution_key, distribution_value in j.items():
-                            f.write("%30s: %s\n"%(distribution_key, distribution_value))
-                    else:
-                        f.write("%30s\n"%(j))
-            else:
-                f.write("%-25s: %s\n" %(k,v))
-        
+        x_dump = json.dumps(x, indent = 4)
+        f.write(x_dump)
