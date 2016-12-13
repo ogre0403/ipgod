05294d1850226bce56de9c8ada7ee8bcb54a5315
unknown <1403035@nchc.org.tw>
2016-12-13 11:02:25 +0800

    add comment of histroy data process

--- crawler/src/Fetcher.py
+++ crawler/src/Fetcher.py
@@ -32,20 +32,19 @@ class Fetcher(threading.Thread):
         self.queue = queue
         self.updateInterval = sec
         if self.updateInterval > 0:
-            schedule.every(self.updateInterval).seconds.do(self.dummy)#fetchNewMetadata
+            schedule.every(self.updateInterval).seconds.do(self.fetchNewMetadata)
 
     def dummy(self):
         """
         dummy function for test
-        """   
-        
+        """
         logger.info(str(threading.get_ident()) + " do repeat")
 
     def process_history(self):
-        # TODO
         """
         process history data since last fetch
         """
+        logger.info(str(threading.get_ident()) + " process hisotry")
         conn = DBUtil.createConnection()
         latestTime = DBUtil.getLastUpdateEpoch(conn)
         DBUtil.closeConnection(conn)
@@ -58,7 +57,6 @@ class Fetcher(threading.Thread):
                 logger.debug(md.getResourceID() + " put to queue")
                 self.queue.put(md)
 
-        logger.info(str(threading.get_ident()) + " process hisotry")
 
     def run(self):
         if self.updateInterval == -1:
@@ -81,23 +79,28 @@ class Fetcher(threading.Thread):
     def findUpdateDataID(self, historyTime= None):
         """
         find all data set which have been updated since self.updateInterval
-
         return array contains dataset id
         """
 
         if historyTime is None:
+            # fetcher thread get new data periodically
             self.timeStr = self.buildQueryTimeStr()
             logger.info("Fetch new metadata since " + self.timeStr)
             r = requests.get(const.MODIFIED_URL_PREFIX + self.timeStr)
         else:
-            # self.timeStr = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            # fetcher thread handle history data
+            # Case 1: First run
+            # Case 2: recovery from failure
             self.timeStr = self.buildQueryTimeStr()
             if historyTime is "NA":
+                # Case 1: The first run scenario, handle all past data
                 logger.info("Fetch new history metadata from scratch")
                 r = requests.get(const.MODIFIED_URL_PREFIX)
             else:
+                # Case 2: Recovery from failure, handle data between failure time and current
                 logger.info("Fetch new history metadata since "+historyTime)
                 r = requests.get(const.MODIFIED_URL_PREFIX + historyTime)
+
         x = json.loads(r.text)
         return x['result']
 
@@ -105,7 +108,6 @@ class Fetcher(threading.Thread):
     def buildQueryTimeStr(self):
         """
         build query time string with format yyyy-mm-dd hh:mm:ss
-
         datetime object value is equal to self.updateInterval ago
         """
         now = datetime.datetime.now()
