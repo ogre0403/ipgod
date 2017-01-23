45d88a53b872e76a14d0c5fcabe64180b1996ad4
Thomas Tsai <thomas@nchc.org.tw>
2017-01-23 23:19:12 +0800

    get dataset file url

--- extractor/get_dataset.py
+++ extractor/get_dataset.py
@@ -7,7 +7,7 @@ import sys
 
 from ckanapi import RemoteCKAN, NotAuthorized, NotFound, ValidationError, CKANAPIError, ServerIncompatibleError
 
-LOGGING_FILE = 'ipgod-od2ckan.log'
+LOGGING_FILE = 'ckan.log'
 logging.basicConfig(filename=LOGGING_FILE,
                     level=logging.INFO,
                     format='%(asctime)s [%(levelname)s] %(filename)s_%(lineno)d  : %(message)s')
@@ -19,26 +19,36 @@ config.read('config.ini')
 class downloadPackage():
     def __init__(self):
         global config
-        url = config.get('od2ckan', 'ckanurl')
-        ckan_key = config.get('od2ckan', 'ckan_key')
+        url = config.get('ckan', 'ckanurl')
+        ckan_key = config.get('ckan', 'ckan_key')
         ua = 'ckanapiexample/1.0 (+http://example.com/my/website)'
         self.ckan = RemoteCKAN(url, apikey=ckan_key, user_agent=ua)
 
     def package(self, packageid):
         package = self.ckan.action.package_show(id=packageid)
-        print package
+        return package
 
     def resource(self, resourceid):
         resource = self.ckan.action.resource_show(id=resourceid)
-        print resource
+        return resource
+
+    def extract_list(self):
+	#test = self.ckan.action.user_show(id='thomas')
+	#test = self.ckan.action.get_news()
+	list_data = self.ckan.action.get_selection()
+	return list_data['list']
+
 
 if __name__ == '__main__': 
-    pkgid = 'lass_data'
-    rid = '8dc0f68b-5175-482b-8aa7-25c202d9f48b
-    if len(sys.argv) > 0:
-        pkgid = sys.argv[1]
 
     ckandata = downloadPackage()
-    ckandata.package(pkgid)
-    ckandata.resource(rid)
+    pkglist = ckandata.extract_list()
+    print pkglist
+    for pkg in pkglist:
+	pkgid = pkg['package']
+	rid = pkg['resource']
+	pkg_data = ckandata.package(pkgid)
+        res_data = ckandata.resource(rid)
+	res_url = res_data['url']
+	print "wget %s" % res_url
 
