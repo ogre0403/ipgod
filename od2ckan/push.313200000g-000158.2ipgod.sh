0af7f480076f17028708e3d229c27d93075b1167
Ceasar Sun <ceasar.sun@gmail.com>
2017-08-31 11:52:07 +0800

    modified:   push.313200000g-000158.2ipgod.sh 	modified:   push.315070700h-000015.2ipgod.sh 	modified:   push.315070700h-000032.2ipgod.sh

--- od2ckan/push.313200000g-000158.2ipgod.sh
+++ od2ckan/push.313200000g-000158.2ipgod.sh
@@ -7,7 +7,7 @@
 
 ### S1.1: CKAN site and required library/toolkit  environment
 
-API_KEY="xxx-xxx-xxx-xxx"  # key for CKAN site
+API_KEY=""  # key for CKAN site
 SITE="http://ipgod.nchc.org.tw"
 LIB_DIR='/opt/ipgod_production/od2ckan/lib'	# need to use push2ckan.py , update2ckan.py
 
@@ -45,16 +45,16 @@ for format in $FORMAT_LIST ; do
     
     resource_id=$(grep -E "^$format" $LATEST_LOG | awk -F ":" '{print $2}')
     if [ -n "$resource_id" ] ;then 
-        last_filename=$(grep -E "^$format" $LATEST_LOG | awk -F ":" '{print $4}')
         last_md5sum=$(grep -E "^$format" $LATEST_LOG | awk -F ":" '{print $3}')
     fi
 
-    echo "'$format':'$resource_id':'${latest_filename}/${last_filename}':'${latest_md5sum}/${last_md5sum}'"
+    echo "'$format':'$resource_id':'${latest_filename}':'${latest_md5sum}/${last_md5sum}'"
+    rm -rf latest-${PACKAGE_ID}.$format ; ln -s $latest_filename latest-${PACKAGE_ID}.$format
 
     if [ -z "$resource_id" ] ; then
         # New add and be as the latest , use : `${LIB_DIR}/push2ckan.py --api_key key --package package-id --name name --file file`
         ${LIB_DIR}/push2ckan.py --api_key $API_KEY --package $PACKAGE_ID --name $latest_filename --file $latest_filename
-        ${LIB_DIR}/push2ckan.py --api_key $API_KEY --package $PACKAGE_ID --name latest-${PACKAGE_ID}.$format --file $latest_filename > $RESOURCE_JSON
+        ${LIB_DIR}/push2ckan.py --api_key $API_KEY --package $PACKAGE_ID --name latest-${PACKAGE_ID}.$format --file latest-${PACKAGE_ID}.$format > $RESOURCE_JSON
 
         resource_id=$(grep -E "^\s*\"id\":" $RESOURCE_JSON | awk -F ":" '{print $2}' |  tr -d ", \"") 
         need_to_update_log='y'
@@ -62,7 +62,7 @@ for format in $FORMAT_LIST ; do
     elif [ ! "$latest_md5sum" = "$last_md5sum"  ] ; then
         # Update resource
         ${LIB_DIR}/push2ckan.py --api_key $API_KEY --package $PACKAGE_ID --name $latest_filename --file $latest_filename
-        ${LIB_DIR}/update2ckan.py --api_key $API_KEY --resource $resource_id --name latest-${PACKAGE_ID}.$format --file $latest_filename
+        ${LIB_DIR}/update2ckan.py --api_key $API_KEY --resource $resource_id --name latest-${PACKAGE_ID}.$format --file latest-${PACKAGE_ID}.$format
         need_to_update_log='y'
     else
 	echo "No need to update latest '$format' file . Skip !"
