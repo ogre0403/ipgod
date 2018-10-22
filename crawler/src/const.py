ca77c293fee04b3c3be99d9a68dd2f8852bc2ad5
wei-yu chen <waue0920@gmail.com>
2018-10-22 15:07:52 +0800

    migrate architecture without DB

--- crawler/src/const.py
+++
@@ -1,6 +0,0 @@
-METADATA_URL_PREFIX = 'http://data.gov.tw/api/v1/rest/dataset/'
-MODIFIED_URL_PREFIX = 'http://data.gov.tw/api/v1/rest/dataset?modified='
-
-# Database and table name
-DB_DATABASE = "ipgod"
-DB_TABLE = "ckan_download"
